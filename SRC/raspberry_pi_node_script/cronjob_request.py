2f6a56ec68ed120f59b2d364ad9513c5b2442c9f
RBEGamer <marcel.ochsendorf@gmail.com>
2015-08-12 00:30:59 +0200

    added new cronjob call method

--- SRC/raspberry_pi_node_script/cronjob_request.py
+++
@@ -1,3 +0,0 @@
-import urllib2
-content = urllib2.urlopen("http://127.0.0.1/smarthome_v2/remote/cron.php").read()
-print content
--- SRC/raspberry_pi_node_script/node.py
+++ SRC/raspberry_pi_node_script/node.py
@@ -5,12 +5,14 @@ read_data = ""
 import serial
 import ConfigParser
 import time
+import subprocess
 
 
 
+subprocess.Popen("cron.py", shell=True)
 
 config = ConfigParser.ConfigParser()
-config.read("/usr/share/adafruit/webide/repositories/my-pi-projects/smart_home_pi/config.ini")
+config.read("config.ini")
 config.sections()
 node_port = int(config.get('config', 'node_port'))
 node_id = config.get('config', 'node_id')
@@ -19,14 +21,12 @@ serial_baud = int(config.get('config', 'serial_baud'))
 serial_port = config.get('config', 'serial_port')
 serial_timeout = int(config.get('config', 'serial_timeout'))
 node_password = config.get('config', 'node_password')
-
 #connect
 #ser = serial.Serial('/dev/ttyACM0',  9600, timeout = 3)
 #ttyAMA0
 ser = serial.Serial('/dev/ttyAMA0', 9600, timeout = 3)
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # Bind the socket to the port
 server_address = (node_ip, node_port)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
@@ -43,7 +43,7 @@ while True:
     
     try:
         print >>sys.stderr, 'connection from', client_address
-
+        
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(1024)
@@ -55,9 +55,9 @@ while True:
                 #read_data = read_data + data
                 try:
                     xmldoc = minidom.parseString(data)
-                    itemlist = xmldoc.getElementsByTagName('pin') 
+                    itemlist = xmldoc.getElementsByTagName('pin')
                     #print len(itemlist)
-                    #print itemlist[0].attributes['value'].value 
+                    #print itemlist[0].attributes['value'].value
                     for s in itemlist :
                         #first = int(s.attributes['output'].value)*1000
                         #last = int(s.attributes['value'].value)
@@ -65,26 +65,26 @@ while True:
                         if s.attributes['token'].value == node_password:
                             print(s.attributes['output'].value  + " ->" + s.attributes['value'].value )
                             ser.write(s.attributes['output'].value  + "-" + s.attributes['value'].value + '\n')
-                            #time.sleep(0.1)
+                #time.sleep(0.1)
                 except:
                     pass
 
 
-            else:
-                print >>sys.stderr, 'no more data from', client_address
-                break
-            
-    finally:
-        # Clean up the connection
-        print read_data
+    else:
+        print >>sys.stderr, 'no more data from', client_address
+            break
+
+finally:
+    # Clean up the connection
+    print read_data
         try:
             xmldoc = minidom.parseString(read_data)
-            itemlist = xmldoc.getElementsByTagName('pin') 
+            itemlist = xmldoc.getElementsByTagName('pin')
             #print len(itemlist)
             #print itemlist[0].attributes['value'].value
             for s in itemlist :
-                print(s.attributes['output'].value  + " ->" + s.attributes['value'].value ) 
-        except:
-            pass
+                print(s.attributes['output'].value  + " ->" + s.attributes['value'].value )
+    except:
+        pass
         read_data = ""
         connection.close()
